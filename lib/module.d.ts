4fcc53c91c6b84aa5b65b2e39565cab3725d919c
Alan Blanchet <alan.blanchet@neovision.fr>
2022-04-14 11:26:07 +0200

    [FIX]Didn't add QueryOptionsProvider
    Forgot to add QueryOptionsProvider in last commit

--- lib/module.d.ts
+++ lib/module.d.ts
@@ -1,3 +1,4 @@
 import CRUD, { CRUDObject } from './components/utils/CRUD';
 import Query from './components/utils/Query';
-export { CRUD, Query, CRUDObject };
+import { QueryOptionsProvider } from './components/utils/QueryOptionsProvider';
+export { CRUD, Query, CRUDObject, QueryOptionsProvider };
