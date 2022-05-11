17dc57a2dabd1e72ab6caf3fdabc401de9ab26d5
Alan Blanchet <alan.blanchet@neovision.fr>
2022-05-11 18:41:37 +0200

    [DEV]Exported useQueryOptions
    Exported useQueryOptions to enable the user to get access to the
    environment variables that allow the connection to the backend

--- src/module.ts
+++ src/module.ts
@@ -1,6 +1,16 @@
 import CRUD, { CRUDObject } from './components/utils/CRUD';
 import Query from './components/utils/Query';
-import { QueryOptionsProvider } from './components/utils/QueryOptionsProvider';
+import {
+  QueryOptionsProvider,
+  useQueryOptions,
+} from './components/utils/QueryOptionsProvider';
 import { request } from './utils/api';
 
-export { CRUD, Query, CRUDObject, QueryOptionsProvider, request };
+export {
+  CRUD,
+  Query,
+  CRUDObject,
+  QueryOptionsProvider,
+  request,
+  useQueryOptions,
+};
