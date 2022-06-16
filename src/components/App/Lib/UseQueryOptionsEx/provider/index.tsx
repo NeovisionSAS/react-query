a28f4b4dd64d3d40d1036debf74d2b8565ca8d28
Alan Blanchet <alan.blanchet@neovision.fr>
2022-06-16 17:17:16 +0200

    [FIX]Wrapped table data in thead & tbody
    Wrapped table data in thead & tbody because of React error message
    in console.

--- src/components/App/Lib/UseQueryOptionsEx/provider/index.tsx
+++ src/components/App/Lib/UseQueryOptionsEx/provider/index.tsx
@@ -9,18 +9,22 @@ export const Provider: FunctionComponent = () => {
 
   return (
     <table>
-      <tr>
-        <th>Key</th>
-        <th>Value</th>
-      </tr>
-      {Object.entries(options).map((entry, i) => {
-        return (
-          <tr key={`provider-${i}`}>
-            <td>{entry[0]}</td>
-            <td>{`${entry[1]}`}</td>
-          </tr>
-        );
-      })}
+      <thead>
+        <tr>
+          <th>Key</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        {Object.entries(options).map((entry, i) => {
+          return (
+            <tr key={`provider-${i}`}>
+              <td>{entry[0]}</td>
+              <td>{`${entry[1]}`}</td>
+            </tr>
+          );
+        })}
+      </tbody>
     </table>
   );
 };
@@ -40,18 +44,22 @@ export const ProviderOverride: FunctionComponent = () => {
 
         return (
           <table>
-            <tr>
-              <th>Key</th>
-              <th>Value</th>
-            </tr>
-            {Object.entries(options).map((entry) => {
-              return (
-                <tr>
-                  <td>{entry[0]}</td>
-                  <td>{`${entry[1]}`}</td>
-                </tr>
-              );
-            })}
+            <thead>
+              <tr>
+                <th>Key</th>
+                <th>Value</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.entries(options).map((entry, i) => {
+                return (
+                  <tr key={`providerOverride-${i}`}>
+                    <td>{entry[0]}</td>
+                    <td>{`${entry[1]}`}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
           </table>
         );
       })()}
